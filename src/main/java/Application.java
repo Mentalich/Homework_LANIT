2c76c79556d0cebcecbd9c4a608e2a7dba49f709
Mentalich <mentalich@gmail.com>
2022-03-23 16:41:35 +0000

    Revert "Update src/main/java/Application.java, src/main/java/animals/Kotik.java"
    This reverts commit 958257dd50ed8aa727aa13978ba1c37696b905c1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,15 +15,12 @@ public class Application {
         //Выводим имя и вес котика Тишки
         System.out.println("Cat's name is " + Tishka.getName() + ", and he weights " + Tishka.getWeight() + " kilos");
         //Выводим сравнение голосов котиков
-        switch (String.valueOf(Tishka.compareVoice(Tom))){
-            case "false":
-                System.out.println("Cats sound different");
-                break;
-            case "true":
-                System.out.println("Cats sound same");
-                break;
-        }
+        System.out.println(compareVoice(Tom,Tishka));
         //Выводим количество котиков
         System.out.println(Tishka.getCount());
+    }
+    //метод сравнения голоса котиков
+    public static boolean compareVoice(Kotik Q, Kotik W){
+        return Q.getVoice()==W.getVoice();
     }
 }
