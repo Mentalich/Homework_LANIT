942d2b96a7d32c7c00dc7058e1402bbb32f1d8ed
Mentalich <mentalich@gmail.com>
2022-04-06 16:00:14 +0000

    Update src/test/java/NegativeCalculatorTest.java, src/test/java/PositiveCalculatorTest.java

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -7,15 +7,18 @@ import org.testng.annotations.Test;
 public class NegativeCalculatorTest {
     // todo: @DataProvider for negativeData
     // todo: negative test
-    @DataProvider(name = "NegativeDataProvider")
-    public Object[][] NegativeDataProvider(){
+    @DataProvider(name = "negativeTest")
+    public Object[][] negativeData(){
         return new Object[][]{
-                {"_","-4","-5","20.0"},
-                {"*","100000000000000000000000000","-5","20.0"}
+                {"_","-4","-5"},
+                {"*","100000000000000000000000000","-5"},
+                {"+","1","1"},
+                {null, null, null}
+
         };
     }
-    @Test(dataProvider = "NegativeDataProvider")
-    public void executeNegTest(String[] params){
+    @Test(dataProvider = "negativeData")
+    public void negativeTest(String[] params){
         Assert.assertEquals(Calculator.execute(params), new CalculatorException());
     }
 
--- src/test/java/PositiveCalculatorTest.java
+++ src/test/java/PositiveCalculatorTest.java
@@ -6,21 +6,24 @@ import org.testng.annotations.Test;
 public class PositiveCalculatorTest {
     // todo: @DataProvider for positiveData
     // todo: positive test
-    @DataProvider(name = "DataInput")
-    public Object[][]DataProviderInput(){
+    @DataProvider(name = "positiveData")
+    public Object[][] positiveData(){
         return new Object[][]{
-                {"+","2","2","4.0"},
-                {"+","2","3","5.0"},
-                {"+","5","5","10.0"},
-                {"-","-3","-3","0.0"},
-                {"+","-3","-3","-6.0"},
-                {"/","-3","-3","1.0"},
-                {"*","-4","-5","20.0"},
+                {"+","2","2",4.0},
+                {"+","2","3",5.0},
+                {"+","5","5",10.0},
+                {"-","-3","-3",0.0},
+                {"+","-3","-3",-6.0},
+                {"/","-3","-3",1.0},
+                {"*","-4","-5",20.0},
+                {"/", "25","4",6.25},
+                {"/", "25","-4",-6.25},
+                {"*", "25","0.2",5.0}
         };
     }
-    @Test(dataProvider = "DataInput")
-    public void executeNegTest(String[] params){
-        String Result = params[3];
-        Assert.assertEquals(Calculator.execute(params),Result);
+    @Test(dataProvider = "positiveData")
+    public void positiveTest(String param, String value1, String value2, double result){
+        String[] input = {param, value1, value2};
+        Assert.assertEquals(Calculator.execute(input),String.valueOf(result));
     }
 }
