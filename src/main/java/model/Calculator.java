851d8a00916be2b7f4a6baeb520e52fdb8a3a366
Mentalich <mentalich@gmail.com>
2022-04-14 13:02:57 +0000

    Update src/test/java/NegativeCalculatorTest.java, src/main/java/model/Calculator.java

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -1,17 +1,29 @@
 package model;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     public static String execute(String[] params) {
+
+        Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+        if (!(pattern.matcher(params[1] + params[2]).matches())){
+            throw new CalculatorException("Не является числом!");
+        }
+        for (int i=0;i<3;i++) {
+            if (params[i] == null) {
+                throw new CalculatorException("Пустая строка!");
+            }
+        }
+
         String operator = params[0];
         double value1;
         double value2;
-        // todo: доработать код, чтобы все негативные тесты проходили успешно
         value1 = Double.valueOf(params[1]);
         value2 = Double.valueOf(params[2]);
         double result = calculate(operator, value1, value2);
         if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
-            throw new CalculatorException("Превышен порmог значений");
+            throw new CalculatorException("Превышен порог значений");
         }
         return String.valueOf(Math.ceil(result * 1000) / 1000);
     }
--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -11,23 +11,14 @@ public class NegativeCalculatorTest {
     public Object[][] negativeData(){
         return new Object[][]{
                 {"_","-4","-5"},//out of bounds symbol
-                {"*","100000000000000000000000000","-5"},//out of bounds number
-                {"/", "5","0"}//divide on zero
+                {"*","100000000000000000000000000","-5000000000000000000000000"},//out of bounds number
+                {"/", "5","0"},//divide on zero
+                {"+","one","two"},
+                {null,null,null}
         };
     }
-
-    @DataProvider(name = "nullData")
-    public Object[]nullData(){
-        return new Object[]{null,null,null};
-    }
-
     @Test(dataProvider = "negativeData", expectedExceptions = CalculatorException.class)
     public void negativeTest(String[] params){
-        Calculator.execute(params);
-    }
-
-    @Test(dataProvider = "nullData", expectedExceptions = NullPointerException.class)
-    public void nullTest(String[] params){
         Calculator.execute(params);
     }
 }
