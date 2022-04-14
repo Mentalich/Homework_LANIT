55a052841b515b5345b919f0c981b3f47fe1d2e0
Mentalich <mentalich@gmail.com>
2022-04-14 20:59:12 +0000

    Update src/test/java/NegativeCalculatorTest.java

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -11,7 +11,7 @@ public class NegativeCalculatorTest {
     public Object[][] negativeData(){
         return new Object[][]{
                 {"_","-4","-5"},//out of bounds symbol
-                {"*","100000000000000000000000000","-5000000000000000000000000"},//out of bounds number
+                {"*","1000000","-50000"},//out of bounds number
                 {"*","2500000","20000"},
                 {"/", "5","0"},//divide on zero
                 {"+","one","two"},
