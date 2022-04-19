750fb34d7038fc0535d45bce0760024eb5fc6bcf
Mentalich <mentalich@gmail.com>
2022-04-19 20:57:18 +0000

    Update src/test/java/PositiveCalculatorTest.java

--- src/test/java/PositiveCalculatorTest.java
+++ src/test/java/PositiveCalculatorTest.java
@@ -20,7 +20,8 @@ public class PositiveCalculatorTest {
                 {"/", "25","-4",-6.25},
                 {"*", "25","0.2",5.0},
                 {"-", "2.0","1.1", 0.9},
-                {"+", "2147483648", "-1", 2147483647.0},
+                {"+", Integer.MAX_VALUE,"0", Double.valueOf(Integer.MAX_VALUE)},
+                {"+", Integer.MIN_VALUE,"0", Double.valueOf(Integer.MIN_VALUE)}
                 
         };
     }
